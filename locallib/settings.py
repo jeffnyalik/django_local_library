8788f2bdda35b062a26c15387a1664c93fc19bb8
Jeff Nyalik <jeffnyak@gmail.com>
2020-03-26 23:54:54 +0300

    change

--- locallib/settings.py
+++ locallib/settings.py
@@ -120,7 +120,7 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/'
 prod_db = dj_database_url.config(conn_max_age=500)
