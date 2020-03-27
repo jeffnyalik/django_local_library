c1adb0a72c887a1d0b85d08b35fd219a58b2f17b
Jeff Nyalik <jeffnyak@gmail.com>
2020-03-27 10:49:44 +0300

    coding shit

--- locallib/settings.py
+++ locallib/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'p&2m%(e52)elv@0i2x+03ddold$i&s4ku%nw6=mmryctk5l(h-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
