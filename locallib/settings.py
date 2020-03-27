02aa0ad3d92b175a94e25a775a5eb4baa195c238
Jeff Nyalik <jeffnyak@gmail.com>
2020-03-27 11:13:26 +0300

    Coding shit

--- locallib/settings.py
+++ locallib/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
 
 #  Add configuration for static files storage using whitenoise
 
