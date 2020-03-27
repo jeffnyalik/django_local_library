5072739f1483adbcc6dc3e2112dd29299c7e68af
Jeff Nyalik <jeffnyak@gmail.com>
2020-03-27 11:58:17 +0300

    status

--- locallib/settings.py
+++ locallib/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
     			os.path.join(BASE_DIR, "static"),
 			]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-# STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
 
 #  Add configuration for static files storage using whitenoise
 
