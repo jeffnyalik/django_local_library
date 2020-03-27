367e11ef00ac6db4bed4414262d751855592f53d
Jeff Nyalik <jeffnyak@gmail.com>
2020-03-27 11:55:56 +0300

    status

--- locallib/wsgi.py
+++ locallib/wsgi.py
@@ -1,9 +1,8 @@
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'locallib.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
